ef4df2aec018fb386e5781be2de4e0c228b525a5
alexio42 <116265425+alexio42@users.noreply.github.com>
2024-06-13 18:12:59 +0100

    Add paragraph directing GFR action
    If statement to inform whether to proceed, omit, or
    check with oncologist. Text highlights gree, red, or
    yellow as visual aid

--- GFR/script.js
+++ GFR/script.js
@@ -1,9 +1,15 @@
-const OMIT = "Scan without contrast and inform Oncologist that contrast media was not given";
-const PROCEED = "Continue with contrast";
-const CHECK = "Only give contrast when the Oncologist has documented that contrast is still necessary";
-const RESULTBOX = document.querySelector("#result");
+const OMIT = "Scan without contrast and inform Oncologist that contrast media was not given.";
+const PROCEED = "Continue with contrast.";
+const CHECK = "Only give contrast when the Oncologist has documented that contrast is still necessary.";
+const LEAFLET = "Give leaflet 'Guidance for patients on how to reduce the risk of the contrast used for their scan impairing their kidney function.' Deliver no more than 75ml of contrast."
+const RESULT_BOX = document.querySelector("#result");
+const INSTRUCT = document.querySelector("#instruct");
+const MAX_CONTRAST = 88;
+const LUNG_UGI = "lungugi";
+const URO_HN = "urohn";
+const OTHER_AREA = "otherarea";
 
-let gender, weight, age, creat, gfr;
+let gender, weight, age, creat, gfr, area;
 
 // add event listener to button and run main() when clicked.
 // const calculate = document.querySelector("#calculate");
@@ -18,6 +24,7 @@ function handleClick() {
     age = document.ptdata["age"].value;
     weight = document.ptdata["weight"].value;
     creat = document.ptdata["creat"].value;
+    area = document.ptdata["area"].value;
     main();
 
 }
@@ -26,9 +33,43 @@ function handleClick() {
 
 function main() {
 
-    console.log(age, gender, weight, creat);
+    
     gfr = calculategfr(gender, weight, age, creat).toFixed(2);
-    RESULTBOX.textContent = gfr;
+    RESULT_BOX.textContent = "GFR = " + gfr;
+    if (gfr >= 50) {
+        INSTRUCT.textContent = PROCEED;
+        INSTRUCT.setAttribute("style", "background-color:green; color:white;");
+    }
+    else if (gfr > 45 && gfr < 50)
+        {
+            INSTRUCT.textContent = CHECK;
+            INSTRUCT.setAttribute("style", "background-color:yellow;");
+        }
+    else if (area == LUNG_UGI) {
+        if (gfr < 45 && gfr > 40) {
+            INSTRUCT.textContent = CHECK + " " + LEAFLET;
+            INSTRUCT.setAttribute("style", "background-color:yellow;");
+        }
+        else {
+            INSTRUCT.textContent = OMIT;
+            INSTRUCT.setAttribute("style", "background-color:red; color:white;");
+            
+        }
+    }
+    else if (area == URO_HN) {
+        if (gfr < 45 && gfr > 30) {
+            INSTRUCT.textContent = CHECK + " " + LEAFLET;
+            INSTRUCT.setAttribute("style", "background-color:yellow;")
+        }
+        else {
+            INSTRUCT.textContent = OMIT;
+            INSTRUCT.setAttribute("style", "background-color:red; color:white;");
+        }
+    }
+    else {
+        INSTRUCT.textContent = OMIT;
+        INSTRUCT.setAttribute("style", "background-color:red; color:white;");
+    }
 };
 
 function calculategfr() {
